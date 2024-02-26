a7f6f032a574082559f2ce1c490fcd71893f333d
oranginalab <tomas@oranginalab.com>
2024-02-26 13:44:06 +0100

    Add an application/acceptance test for /greet/{name} endpoint based on HttpClient component.

---
+++ tests/Application/Greeter/GreetEndpointTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Tests\Application\Greeter;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+
+class GreetEndpointTest extends TestCase
+{
+
+    public function test_we_can_greet_you(): void
+    {
+        $client = HttpClient::create();
+        try {
+            $baseUri = 'http://webserver:80';
+            $response = $client->request('GET', $baseUri . '/greet/vonTrotta');
+            $response_obj = json_decode($response->getContent());
+        } catch (ClientExceptionInterface|RedirectionExceptionInterface|ServerExceptionInterface|TransportExceptionInterface $e) {
+            $this->fail($e->getMessage());
+        }
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('Hello, vonTrotta!', $response_obj->message);
+    }
+}
