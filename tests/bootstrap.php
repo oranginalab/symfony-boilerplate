2d54d469fe710ed5f629feb4cc7a9f9237684dd1
oranginalab <tomas@oranginalab.com>
2024-03-01 18:55:16 +0100

    conf: Suggested code to run commands before starting testsuite execution. We will use it to create the test database when we find a proper way to destroy it when we are done.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,3 +9,24 @@ if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 }
+
+/*
+// Create test database
+passthru(
+    sprintf(
+        'APP_ENV=%s php "%s/../bin/console" --env=test doctrine:database:create',
+        $_ENV['APP_ENV'],
+        __DIR__
+    )
+);
+
+// Create test database tables
+passthru(
+    sprintf(
+        'APP_ENV=%s php "%s/../bin/console" --env=test doctrine:scheme:create',
+        $_ENV['APP_ENV'],
+        __DIR__
+    )
+);
+
+*/
